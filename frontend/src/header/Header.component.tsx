6c1adcfe38ae046764201a0ed5e7f77d93768d4a
PixelsPioneer <andriyvoloshyk228@gmail.com>
2025-02-05 11:45:17 +0200

    fix(header): fix header and change link to button

--- frontend/src/header/Header.component.tsx
+++ frontend/src/header/Header.component.tsx
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { Themes } from '../constants/constants';
+import { AuthModal } from '../modalWindow/AuthModal';
 import { useTokenContext } from '../tokenContext/TokenContext';
 import styles from './header.module.css';
 
 interface HeaderProps {
   toggleTheme: () => void;
   theme: Themes;
+  onSignIn: () => void;
+  onSignUp: () => void;
 }
 
 export const Header: React.FC<HeaderProps> = ({ toggleTheme, theme }) => {
   const { token } = useTokenContext();
+  const navigate = useNavigate();
   const [currentTheme, setCurrentTheme] = useState<Themes>(theme);
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(true);
 
   useEffect(() => {
     setCurrentTheme(theme);
   }, [theme]);
+
+  const openAuthModal = (signIn: boolean) => {
+    setIsSignIn(signIn);
+    setIsAuthModalOpen(true);
+  };
+
+  const closeAuthModal = () => setIsAuthModalOpen(false);
+
+  const handleSignOut = () => {
+    navigate('/signout');
+  };
+
+  const handleProduct = () => {
+    navigate('/');
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.logo}>Scraper</div>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <Link to="/" className={styles.navLink}>
-              Products
-            </Link>
-          </li>
           {!token && (
             <li className={styles.navItem}>
-              <Link to="/signup" className={styles.navLink}>
-                Sign Up
-              </Link>
-              <Link to="/signin" className={styles.navLink}>
-                Sign In
-              </Link>
+              <button onClick={() => openAuthModal(true)} className={styles.AuthButton}>
+                Auth
+              </button>
             </li>
           )}
           {token && (
             <li className={styles.navItem}>
-              <Link to="/signout" className={styles.navLink}>
+              <button onClick={handleProduct} className={styles.productButton}>
+                Product
+              </button>
+              <button onClick={handleSignOut} className={styles.signOutButton}>
                 Sign Out
-              </Link>
+              </button>
             </li>
           )}
         </ul>
@@ -54,6 +71,8 @@ export const Header: React.FC<HeaderProps> = ({ toggleTheme, theme }) => {
           <div className={styles.slider}></div>
         </div>
       </div>
+
+      <AuthModal isOpen={isAuthModalOpen} onClose={closeAuthModal} isSignIn={isSignIn} />
     </header>
   );
 };
