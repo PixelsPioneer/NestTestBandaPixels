a5151b0ca2c247e19bc4919b88a7698d43e3ddd7
PixelsPioneer <andriyvoloshyk228@gmail.com>
2025-03-13 14:25:44 +0200

    fix(product): fix class product pagination

--- back-nest/src/products/model/paginatination-return.model.ts
+++ back-nest/src/products/model/paginatination-return.model.ts
@@ -1,23 +1,20 @@
 import { ProductModel } from './product.model';
 
 export class PaginatedResponseModel {
-  products: ProductModel[];
-  totalPages: number;
-  currentPage: number;
-  hasNextPage: boolean;
-  totalProducts: number;
-
   constructor(
-    products: ProductModel[],
-    totalPages: number,
-    currentPage: number,
-    hasNextPage: boolean,
-    totalProducts: number,
-  ) {
-    this.products = products;
-    this.totalPages = totalPages;
-    this.currentPage = currentPage;
-    this.hasNextPage = hasNextPage;
-    this.totalProducts = totalProducts;
+    public products: ProductModel[],
+    public totalPages: number,
+    public currentPage: number,
+    public hasNextPage: boolean,
+    public totalProducts: number,
+  ) {}
+  static create(data: Partial<PaginatedResponseModel>): PaginatedResponseModel {
+    return new PaginatedResponseModel(
+      data.products ?? [],
+      data.totalPages ?? 1,
+      data.currentPage ?? 1,
+      data.hasNextPage ?? false,
+      data.totalProducts ?? 0,
+    );
   }
 }
