7ed877c4d92b466067c5ee55304eba7a5126656f
PixelsPioneer <andriyvoloshyk228@gmail.com>
2025-03-20 16:21:28 +0200

    feat(database): create and modify tables for scraped categories, subcategories, and sections

--- back-nest/prisma/prisma.service.ts
+++ back-nest/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -6,11 +11,16 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
     await this.$connect();
+    const dbUrl = process.env.DATABASE_URL;
+    this.logger.log(`Conected to the database ${dbUrl}`);
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    this.logger.log('Disconnected from the database');
   }
 }
