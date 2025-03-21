e287da35bb0a66eb3e82ebebabfbc8c7d775f3e8
PixelsPioneer <andriyvoloshyk228@gmail.com>
2025-03-21 12:39:34 +0200

    feat(scrapers): streamline catalog and scraping methods by removing redundant logging and checks

--- back-nest/src/sсrapers/scrapers.controller.ts
+++ back-nest/src/sсrapers/scrapers.controller.ts
@@ -18,53 +18,20 @@ export class ScraperController {
   @HttpCode(200)
   @Get('catalog')
   async catalogService(): Promise<ScrapedCategoryDto[]> {
-    this.logger.log('Start to get catalog service...');
-    const scrapedCatalog = await this.catalogScraperService.scrapeCategories();
-
-    if (!scrapedCatalog || scrapedCatalog.length === 0) {
-      this.logger.warn('No categories found in scraped catalog from Telemart.');
-      return [];
-    }
     return this.catalogScraperService.scrapeAndSaveCategories();
   }
 
   @HttpCode(204)
   @Get('scrape-telemart')
   async scrapeTelemart(): Promise<void> {
-    this.logger.log('Starting Telemart scraping...');
-    const scrapedProducts = await this.telemartScraperService.scrapeTelemart();
-
-    if (!scrapedProducts || scrapedProducts.length === 0) {
-      this.logger.warn('No products were scraped from Telemart.');
-      return;
-    }
-
-    this.logger.log(
-      `Scraped ${scrapedProducts.length} products from Telemart.`,
-    );
-
     await this.telemartScraperService.scrapeAndSaveTelemartProducts();
-
     return;
   }
 
   @HttpCode(204)
   @Get('scrape-rozetka')
   async scrapeRozetka(): Promise<void> {
-    this.logger.log('Starting to scrape products from Rozetka...');
-
-    const scrapedProducts =
-      await this.rozetkaScraperService.scrapeRozetkaProducts();
-
-    if (!scrapedProducts || scrapedProducts.length === 0) {
-      this.logger.warn('No products were scraped from Rozetka.');
-      return;
-    }
-
-    this.logger.log(`Scraped ${scrapedProducts.length} products from Rozetka.`);
-
     await this.rozetkaScraperService.scrapeAndSaveRozetkaProducts();
-
     return;
   }
 }
