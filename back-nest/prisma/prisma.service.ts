b1b7e57222a4f93a6d936ccdff0c69059899466c
PixelsPioneer <andriyvoloshyk228@gmail.com>
2025-03-24 15:36:43 +0200

    refactor(prisma): remove ConfigService dependency and related logging

--- back-nest/prisma/prisma.service.ts
+++ back-nest/prisma/prisma.service.ts
@@ -5,26 +5,19 @@ import {
   Logger,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor(private readonly configService: ConfigService) {
-    super();
-  }
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
     await this.$connect();
-    const _dbUrl = this.configService.getOrThrow<string>('DATABASE_URL');
-    this.logger.log('Connected to the database');
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
-    this.logger.log('Disconnected from the database');
   }
 }
